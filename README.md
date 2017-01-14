ea8cf8e13b2a262f39e54a8455834ba13fc31e96
Katochimoto <bufpost@yandex.ru>
2017-01-14 18:52:54 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # rollup-plugin-less2
 A rollup plugin for less files, support css modules
+
+```js
+// rollup.config.js
+import RollupPluginLess2 from 'rollup-plugin-less2';
+import LessPluginCssModules from 'less-plugin-css-modules';
+
+// ...
+
+export default {
+  // ...
+  plugins: [
+    RollupPluginLess2({
+      output: false,
+      cssModules: true,
+      options: {
+        plugins: [
+          new LessPluginCssModules({
+            mode: 'local',
+            hashPrefix: 'test',
+            generateScopedName: '[local]___[hash:base64:5]'
+          })
+        ]
+      }
+    })
+  ]
+};
+```
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const rollup = require('rollup');
+const LessPluginCssModules = require('less-plugin-css-modules');
 const RollupPluginLess2 = require('../src/index.js');
 
 describe('rollup-plugin-less2', function () {
@@ -8,14 +9,23 @@ describe('rollup-plugin-less2', function () {
       entry: path.join(__dirname, 'samples', 'main.js'),
       plugins: [
         RollupPluginLess2.default({
-          cssModules: false,
-          output: false // path.join(__dirname, 'samples', 'style.css')
+          cssModules: true,
+          output: false, // path.join(__dirname, 'samples', 'style.css')
+          options: {
+            plugins: [
+              new LessPluginCssModules.default({
+                mode: 'local',
+                hashPrefix: 'test',
+                generateScopedName: '[local]___[hash:base64:5]'
+              })
+            ]
+          }
         })
       ]
     }).then(bundle => {
       const result = bundle.generate({ sourceMap: false, format: 'cjs' });
 
-      // console.log('>>>>', result.code);
+      console.log('>>>>', result.code);
     });
   });
 });
