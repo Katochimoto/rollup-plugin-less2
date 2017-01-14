3d0ab32abdf4819da58d6af0405aab9d895d9ebf
Katochimoto <bufpost@yandex.ru>
2017-01-14 19:10:37 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ export default function RollupPluginLess2 ({
         return;
       }
 
+      options.filename = id;
       let resultCss = await less.render(code, options).then(function (output) {
         return output.css;
       });
